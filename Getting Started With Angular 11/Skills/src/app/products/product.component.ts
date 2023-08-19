0a547928fda9b813c3435606f42dfc9d13591d1a
yogekgagdev <yogekgarg17@gmail.com>
2023-08-20 01:14:40 +0530

    Iterating through complex objects with *ngFor

--- Getting Started With Angular 11/Skills/src/app/products/product.component.ts
+++ Getting Started With Angular 11/Skills/src/app/products/product.component.ts
@@ -6,11 +6,30 @@ declare const genRandomNumbers: any;
     template: `<div *ngIf='ProductShow'>Product Works</div>
                <div>{{data}}</div>
                <br><br>
-               <div><li *ngFor='let product of productsArray'>{{product}}</li></div>`,
+               <div><li *ngFor='let product of productsArray'>{{product}}</li></div><br>
+               <div>Iterating Through Complex Objects</div>
+               <li *ngFor="let person of peoplesArray">{{person.name}}</li>
+               `,
     styles: ['li{color:green; margin-top:10px}']
 })
 export class ProductComponent {
     ProductShow = true;
     productsArray = ["Motorcycle", "Bike","Car","Scooter"];
+    // Iterating Through Complex Objects
+    peoplesArray =[
+        {
+            name: "Yogek Garg",
+            id:"123131"
+        },
+        {
+            name: "Ankit",
+            id:"123131"
+        },
+        {
+            name: "Abhinav",
+            id:"123131"
+        }
+
+    ]  
     data = <[]>genRandomNumbers();
 }
